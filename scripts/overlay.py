7737e71952df7ebfe8de23d8ea2328b5fd7c87ec
Hrishi-Shekhar <hrishi08072005@gmail.com>
2025-05-28 18:03:19 +0530

    Modified overlay script with output annotations

--- scripts/overlay.py
+++ scripts/overlay.py
@@ -29,7 +29,7 @@ def overlay_foreground_on_background(
 
         composite = bg_img.copy()
         placed_boxes = []
-
+        annotation_lines = []
         num_foregrounds_per_bg = random.randint(min_foregrounds_per_bg, max_foregrounds_per_bg)
 
         for _ in range(num_foregrounds_per_bg):
@@ -51,12 +51,32 @@ def overlay_foreground_on_background(
                 if not any(check_overlap(new_box, b) for b in placed_boxes):
                     placed_boxes.append(new_box)
                     composite.alpha_composite(fg_img, dest=(x, y))
+                    x_center = (x + fg_img.width/2)/bg_width
+                    y_center = (y + fg_img.height/2)/bg_height
+                    width_norm = fg_img.width/bg_width
+                    height_norm = fg_img.height/bg_height
+                    annotation_lines.append(f"0 {x_center:.6f} {y_center:.6f} {width_norm:.6f} {height_norm:.6f}")
+                    
                     break
             else:
                 print(f"Could not place {fg_file} without overlap after {max_attempts} attempts.")
 
-        output_path = os.path.join(output_dir, f"composite_{i+1}.jpg")
+        output_path = os.path.join(output_dir, f"composites/composite_{i+1}.jpg")
         composite.convert("RGB").save(output_path)
         print(f"Saved: {output_path}")
+
+        output_label_path = os.path.join(output_dir,f"annotations/composite_{i+1}.txt")
+        with open(output_label_path,"w") as f:
+            f.write("\n".join(annotation_lines))
+
+if __name__ == "__main__":
+    cropped_nobg_dir = r"data/Skin_cancer_augmentation/intermediate/cropped_nobg"
+    search_keyword = "medical skin background, macro skin surface, dermatology texture, neutral pinkish skin, no moles or lesions, high-quality close-up"
+    background_folder = r"data/Skin_cancer_augmentation/backgrounds/Web_scraping"
+    overlay_foreground_on_background(
+        cropped_nobg_dir,
+        backgrounds_dir= os.path.join(background_folder,search_keyword),
+        output_dir=r"data/Skin_cancer_augmentation/output"
+    )
 
 
